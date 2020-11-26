29017bbe801fa95c88be8e54760e19d0f404e8ef
Jesus Pacheco <je.pacheco2311@gmail.com>
2020-11-26 18:49:33 +0100

    se agrega libreria para mensajes estilizados y se valida si gano o perdio

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ const ULTIMO_NIVEL = 10
 
 class Juego {
     constructor() {
+        this.inicializar = this.inicializar.bind(this)
         this.inicializar()
         this.generarSecuencia()
         setTimeout(this.siguienteNivel, 500)
@@ -15,13 +16,21 @@ class Juego {
     inicializar() {
         this.elegirColor = this.elegirColor.bind(this)
         this.siguienteNivel = this.siguienteNivel.bind(this)
-        btnEmpezar.classList.add('hide')
+        this.toggleBtnEmpezar()
         this.nivel = 1
         this.colores = {
             celeste: celeste,
             violeta,
             naranja,
             verde
+        }
+    }
+
+    toggleBtnEmpezar() {
+        if (btnEmpezar.classList.contains('hide')) {
+            btnEmpezar.classList.remove('hide')
+        } else {
+            btnEmpezar.classList.add('hide')
         }
     }
 
@@ -101,14 +110,27 @@ class Juego {
                     this.nivel++
                         //this.eliminarEventosClick()
                         if (this.nivel === (ULTIMO_NIVEL + 1)) {
-                            //Ganaste
+                            this.ganoElJuego()
                         } else {
                             setTimeout(this.siguienteNivel, 1500)
                         }
                 }
         } else {
-            //Perdio
+            this.perdioElJuego()
         }
+    }
+
+    ganoElJuego() {
+        swal('Platzi', 'Felicitaciones, Ganaste!', 'success')
+            .then(this.inicializar)
+    }
+
+    perdioElJuego() {
+        swal('Platzi', 'Lo siento, perdiste :(', 'error')
+            .then(() => {
+                this.eliminarEventosClick()
+                this.inicializar()
+            })
     }
 
 }
