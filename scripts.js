e94951e8b77794461aaa127a47b4e47fcb904552
Jesus Pacheco <je.pacheco2311@gmail.com>
2020-11-26 16:53:17 +0100

    acciones de click, bind para el thsi de los colores y secuencia de encendido

--- scripts.js
+++ scripts.js
@@ -7,14 +7,73 @@ const btnEmpezar = document.getElementById('btnEmpezar')
 class Juego {
     constructor() {
         this.inicializar()
+        this.generarSecuencia()
+        this.siguienteNivel()
     }
 
     inicializar() {
+        this.elegirColor = this.elegirColor.bind(this)
         btnEmpezar.classList.add('hide')
+        this.nivel = 1
+        this.colores = {
+            celeste: celeste,
+            violeta,
+            naranja,
+            verde
+        }
+    }
+
+    generarSecuencia() {
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+    }
+
+    siguienteNivel() {
+        this.iluminarSecuencia()
+        this.agregarEventosClick()
+    }
+
+    transformarNroAColor(numero) {
+        switch (numero) {
+            case 0:
+                return 'celeste'
+            case 1:
+                return 'violeta'
+            case 2:
+                return 'naranja'
+            case 3:
+                return 'verde'
+        }
+    }
+
+    iluminarSecuencia() {
+        for (let i = 0; i < this.nivel; i++) {
+            const color = this.transformarNroAColor(this.secuencia[i])
+            setTimeout(() => this.iluminarColor(color), 1000 * i)
+        }
+    }
+
+    iluminarColor(color) {
+        this.colores[color].classList.add('light')
+        setTimeout(() => this.apagarColor(color), 350)
+    }
+
+    apagarColor(color) {
+        this.colores[color].classList.remove('light')
+    }
+
+    agregarEventosClick() {
+        this.colores.celeste.addEventListener('click', this.elegirColor)
+        this.colores.verde.addEventListener('click', this.elegirColor)
+        this.colores.violeta.addEventListener('click', this.elegirColor)
+        this.colores.naranja.addEventListener('click', this.elegirColor)
+    }
+
+    elegirColor(ev) {
+
     }
 
 }
 
 function empezarJuego() {
-    var juego = new Juego()
+    window.juego = new Juego()
 }
